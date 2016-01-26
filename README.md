29676012ab11eb6f19bf3483ba3b27cac5132a31
Neal Granger <neal@metalab.co>
2016-01-26 02:47:09 -0800

    Fix build.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Request Combinator
 
 Functional tool for generating requests.
+
+[![Build Status](https://img.shields.io/travis/nealgranger/request-combinator/master.svg)](https://travis-ci.org/nealgranger/request-combinator)
+[![Coverage Status](https://img.shields.io/coveralls/nealgranger/request-combinator/master.svg)](https://coveralls.io/github/nealgranger/request-combinator)
--- src/request/url.js
+++ src/request/url.js
@@ -1,23 +1,23 @@
-import { assign } from './index';
-import * as url from '../url';
+import * as request from './index';
+import * as _url from '../url';
 
-export const _lift = (func) => (request = {}) => {
-  const { url } = request;
-  return assign({ url: func(url)})(request);
+export const _lift = (func) => (_request = {}) => {
+  const { _url } = _request;
+  return request.assign({ url: func(_url)})(_request);
 };
 
-export const url = _lift(url.set);
+export const url = _lift(_url.set);
 export default url;
-export const assign = _lift(url.assign);
-export const protocol = _lift(url.protocol);
-export const auth = _lift(url.auth);
-export const host = _lift(url.host);
-export const hostname = _lift(url.hostname);
-export const port = _lift(url.port);
-export const path = _lift(url.pathname);
-export const rebaseUrlPath = _lift(url.rebasePath);
-export const joinPath = _lift(url.joinPath);
-export const hash = _lift(url.hash);
-export const search = _lift(url.search);
-export const rebase = _lift(url.rebase);
-export const formatUrl = _lift(url.format);
+export const assign = _lift(_url.assign);
+export const protocol = _lift(_url.protocol);
+export const auth = _lift(_url.auth);
+export const host = _lift(_url.host);
+export const hostname = _lift(_url.hostname);
+export const port = _lift(_url.port);
+export const path = _lift(_url.pathname);
+export const rebaseUrlPath = _lift(_url.rebasePath);
+export const joinPath = _lift(_url.joinPath);
+export const hash = _lift(_url.hash);
+export const search = _lift(_url.search);
+export const rebase = _lift(_url.rebase);
+export const formatUrl = _lift(_url.format);
